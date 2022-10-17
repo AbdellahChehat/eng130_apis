db61d95137493c37c94e067c11efe4238d8412de
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-17 14:41:04 +0100

    message

--- python_api.py
+++ python_api.py
@@ -2,4 +2,11 @@
 # Install requests
 # pip install request
 
-import pip
+
+import requests
+
+request_bbc_status_code = requests.get("https://www.bbc.co.uk/iplayer/live/bbcnews")
+
+# Check the outcome of our api call
+
+print(request_bbc_status_code.status_code)
