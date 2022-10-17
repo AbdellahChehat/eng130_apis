7a01740274e0d1b26fd379c4d6ac24685972b858
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-17 14:59:13 +0100

    Updated

--- python_api.py
+++ python_api.py
@@ -9,4 +9,17 @@ request_bbc_status_code = requests.get("https://www.bbc.co.uk/iplayer/live/bbcne
 
 # Check the outcome of our api call
 
-print(request_bbc_status_code.status_code)
+# print(request_bbc_status_code.status_code)
+# # let's check the header
+# print(request_bbc_status_code.headers)
+# # lets check the contents available
+# print(request_bbc_status_code.content)
+
+# print the status code with success message
+
+# print unsucessful if status code is  not 200
+
+if request_bbc_status_code.status_code == 200:
+    print("Success")
+else:
+    print("Unsuccessful")
